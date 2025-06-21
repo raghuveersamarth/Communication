ab70edf1a4977403d17ad7806dc1ea5ce55e794c
raghuveersamarth <raghuveersamarth0@gmail.com>
2025-06-21 12:39:20 +0530

    billing page

--- app/Courses/[id]/page.js
+++ app/Courses/[id]/page.js
@@ -34,3 +34,4 @@ const Courses = () => {
 };
 
 export default Courses;
+
--- app/dashboard/Billing/page.js
+++ app/dashboard/Billing/page.js
@@ -13,12 +13,77 @@ const inter = Inter({
 });
 
 const BillingForm = () => {
-
+  const UserActivation = (e) => {
+    let ndata = Object.fromEntries(e)
+    
+  };
 
   return (
-    <div className={`${inter.className}`}>
-  
-    </div>
+    <>
+      <div
+        className={`${inter.className} mt-16 border border-gray-700 rounded-2xl shadow-lg  hover:shadow-amber-500/10 transition-all duration-300 hover:border-amber-500/30 h-[600px] w-[600px] bg-[#101010] flex flex-col items-center p-6 justify-center`}
+      >
+        <h1 className="mb-12 font-bold text-3xl">Create an account</h1>
+        <form
+          action={UserActivation}
+          className=" flex flex-col items-center gap-4 "
+        >
+          <label>
+            <RevealOnScroll delay={0.2}>
+              <input
+                className="mb-4 p-2  w-[400px] bg-[#202020] text-white border border-gray-700 rounded-2xl shadow-lg  hover:shadow-amber-500/10 transition-all duration-300 hover:border-amber-500/30 "
+                name="username"
+                type="text"
+                defaultValue={""}
+                placeholder="Enter your username"
+              />
+            </RevealOnScroll>
+          </label>
+          <label>
+            <RevealOnScroll delay={0.4}>
+              <input
+                className="mb-4 p-2  w-[400px] bg-[#202020] text-white border border-gray-700 rounded-2xl shadow-lg  hover:shadow-amber-500/10 transition-all duration-300 hover:border-amber-500/30 "
+                name="email"
+                type="text"
+                defaultValue={""}
+                placeholder="Enter your email"
+              />
+            </RevealOnScroll>
+          </label>
+          <label>
+            <RevealOnScroll delay={0.6}>
+              <input
+                className="mb-4 p-2  w-[400px] bg-[#202020] text-white border border-gray-700 rounded-2xl shadow-lg  hover:shadow-amber-500/10 transition-all duration-300 hover:border-amber-500/30 "
+                name="password"
+                type="text"
+                defaultValue={""}
+                placeholder="Create a Password"
+              />
+            </RevealOnScroll>
+          </label>
+          <label>
+            <RevealOnScroll delay={0.8}>
+              <input
+                className="mb-4 p-2  w-[400px] bg-[#202020] text-white border border-gray-700 rounded-2xl shadow-lg  hover:shadow-amber-500/10 transition-all duration-300 hover:border-amber-500/30 "
+                name="confirmPassword"
+                type="text"
+                defaultValue={""}
+                placeholder="Confirm Password"
+              />
+            </RevealOnScroll>
+          </label>
+          <RevealOnScroll delay={1.0}>
+            <button
+              type="submit"
+              className="border text-center border-amber-500 text-white  py-3 rounded-lg hover:bg-amber-600 transition-colors font-medium px-12"
+            >
+              <span className="text-gray-400 text-lg line-through">₹24000</span>
+              <span className="text-3xl text-amber-400">₹18397</span>
+            </button>
+          </RevealOnScroll>
+        </form>
+      </div>
+    </>
   );
 };
 
